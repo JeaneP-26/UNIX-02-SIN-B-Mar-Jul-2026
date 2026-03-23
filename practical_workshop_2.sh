e554ff22883227f359d8f7776ab3af9b88906b50
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-03-23 13:23:12 +0000

    practical_workshop_8

--- practical_workshop_2.sh
+++ practical_workshop_2.sh
@@ -1,16 +1,16 @@
-uname -a
-which gpg
-gpg --version
-gpg --full-generate-key
-gpg --list-keys
-gpg --armor --export jeanellaparedes@gmail.com > mi_llave_publica.asc
-ls mi_llave_publica.asc
-gpg --armor --export
-gpg --list-secret-keys --keyid-format=long
-gpg --armor --export-secret-keys 3B2DD130907A2F2E
-gpg --import mi_compa_llave_publica.asc
-gpg --list-keys
-$ echo "este mensaje es secreto" > doc_no_cifrado.txt
-ls 
-cat doc_no_cifrado.txt
-gpg --output doc_cifrado.txt --encrypt --recipient laquisnancela@gmail.com doc_no_cifrado.txt
+uname -a # Muestra información completa del sistema
+which gpg  # Indica la ruta donde está instalado el comando gpg en el sistema
+gpg --version # Muestra la versión de GPG instalada
+gpg --full-generate-key # Inicia el proceso completo para crear claves (pública y privada)
+gpg --list-keys # Lista todas las claves públicas almacenadas en el sistema
+gpg --armor --export jeanellaparedes@gmail.com > mi_llave_publica.asc # Exporta la clave pública en formato legible y la guarda en un archivo
+ls mi_llave_publica.asc # Verifica que el archivo de la clave pública se haya creado
+gpg --armor --export # Muestra todas las claves públicas en la terminal
+gpg --list-secret-keys --keyid-format=long # Lista las claves privadas
+gpg --armor --export-secret-keys 3B2DD130907A2F2E # Exporta la clave privada 
+gpg --import mi_compa_llave_publica.asc # Importa la clave pública mi companero en mi sistema
+gpg --list-keys # Verifica que la clave importada esté en la lista de claves públicas
+$ echo "este mensaje es secreto" > doc_no_cifrado.txt # Crea un archivo de texto con un mensaje sin cifrar
+ls # Lista los archivos
+cat doc_no_cifrado.txt # Muestra el contenido del archivo sin cifrar
+gpg --output doc_cifrado.txt --encrypt --recipient laquisnancela@gmail.com doc_no_cifrado.txt # Cifra el archivo usando la clave pública del destinatario y genera un archivo cifrado
