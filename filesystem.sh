3b223ededc29001178cc375244f2d33d33a29c31
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-03-25 13:18:30 +0000

    file_system_go_to_the_system_List_forlders_files_Show_the_inodes

--- filesystem.sh
+++ filesystem.sh
@@ -1,1 +1,4 @@
 pwd #Print Working Directory, Show the path of the current directory
+cd / #Go to the system’s root directory
+ls -F #List files and folders, adding a symbol at the end to show their type
+ls -i #Show the inodes
