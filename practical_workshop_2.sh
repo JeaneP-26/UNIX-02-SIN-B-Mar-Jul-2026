41ef406dc25f250c83404f923d16ba1f11d7ee0a
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-03-25 01:52:47 +0000

    practical_workshop_20_Verify_the_separate_signature_of_our_partner

--- practical_workshop_2.sh
+++ practical_workshop_2.sh
@@ -25,3 +25,4 @@ gpg --verify doc_no_cifrado_firmado_1.txt #Return to verify the signature of our
 gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt #Sign the document in binary
 gpg --verify doc_no_cifrado_firmado_binario_1.txt #Verify the signature in binary of our partner
 gpg --output firma_separada_doc_no_cifrado.sig --detach-sign doc_no_cifrado.txt #Sign a document with a separate document
+gpg --verify firma_separado_doc_no_cifrado_1.sig doc_no_cifrado_1.txt #Verify the separate signature of our partner
