7d7628ed33f62f20b7872fbe3d3ac0219d581b5e
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-03-25 01:29:58 +0000

    practical_workshop_18_Traduction_Firsts_Commands

--- practical_workshop_2.sh
+++ practical_workshop_2.sh
@@ -1,26 +1,27 @@
-uname -a # Muestra información completa del sistema
-which gpg  # Indica la ruta donde está instalado el comando gpg en el sistema
-gpg --version # Muestra la versión de GPG instalada
-gpg --full-generate-key # Inicia el proceso completo para crear claves (pública y privada)
-gpg --list-keys # Lista todas las claves públicas almacenadas en el sistema
-gpg --armor --export jeanellaparedes@gmail.com > mi_llave_publica.asc # Exporta la clave pública en formato legible y la guarda en un archivo
-ls mi_llave_publica.asc # Verifica que el archivo de la clave pública se haya creado
-gpg --armor --export # Muestra todas las claves públicas en la terminal
-gpg --list-secret-keys --keyid-format=long # Lista las claves privadas
-gpg --armor --export-secret-keys 3B2DD130907A2F2E # Exporta la clave privada 
-gpg --import mi_compa_llave_publica.asc # Importa la clave pública mi companero en mi sistema
-gpg --list-keys # Verifica que la clave importada esté en la lista de claves públicas
-$ echo "este mensaje es secreto" > doc_no_cifrado.txt # Crea un archivo de texto con un mensaje sin cifrar
-ls # Lista los archivos
-cat doc_no_cifrado.txt # Muestra el contenido del archivo sin cifrar
-gpg --output doc_cifrado.txt --encrypt --recipient laquisnancela@gmail.com doc_no_cifrado.txt # Cifra el archivo usando la clave pública del destinatario y genera un archivo cifrado
-gpg --decrypt doc_cifrado_1.txt #Decifra el documento enviado por mi companero
-gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #Firma el archivo con gnupg/clearsign
-ls # Lista todos los archivos
-cat doc_no_cifrado_firmado.txt # Muestra en pantalla el contenido del archivo y su firma digital en el texto
+uname -a # Displays complete system information
+which gpg  # Indicates the path where the gpg command is installed on the system
+gpg --version # Shows the installed GPG version
+gpg --full-generate-key # Starts the full process to create a key pair (public and private)
+gpg --list-keys # Lists all public keys stored in the system
+gpg --armor --export jeanellaparedes@gmail.com > mi_llave_publica.asc #Exports the public key in a readable format and saves it to a file
+ls mi_llave_publica.asc # Verifies that the public key file has been created
+gpg --armor --export # Displays all public keys in the terminal
+gpg --list-secret-keys --keyid-format=long # Lists the private keys
+gpg --armor --export-secret-keys 3B2DD130907A2F2E # Exports the private key
+gpg --import mi_compa_llave_publica.asc # Imports my partner public key into my system
+gpg --list-keys # Verifies that the imported key is in the list of public keys
+$ echo "este mensaje es secreto" > doc_no_cifrado.txt # Creates a text file with an unencrypted message
+ls # Lists all files
+cat doc_no_cifrado.txt # Displays the content of the file without encryption
+gpg --output doc_cifrado.txt --encrypt --recipient laquisnancela@gmail.com doc_no_cifrado.txt # Encrypts the file using the recipient’s public key and generates an encrypted file
+gpg --decrypt doc_cifrado_1.txt #Decrypts the document sent by my partner
+gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #Signs the file using GnuPG with clearsign
+ls # Lists all files
+cat doc_no_cifrado_firmado.txt # Displays the content of the file on screen along with its digital signature in the text.
 gpg --verify doc_no_cifrado_firmado_1.txt #Verify the signature of our partner 
 gpg --edit-key 9B13E2392E210FF7A3A0EF4BDFA1A9B4A5C355EE #Web of Trust to our partner
 gpg --sign-key DFA1A9B4A5C355EE #Sign the key of our decision
 gpg --verify doc_no_cifrado_firmado_1.txt #Return to verify the signature of our partner
 gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt #Sign the document in binary
 gpg --verify doc_no_cifrado_firmado_binario_1.txt #Verify the signature in binary of our partner
+
