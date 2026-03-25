b6f02831582b5dc9ff164746d66bd8acc04c722e
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-03-25 13:44:39 +0000

    file_system_Create_a_new_file_Show_the_information_of_the_file

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,5 @@ pwd #Print Working Directory, Show the path of the current directory
 cd / #Go to the system’s root directory
 ls -F #List files and folders, adding a symbol at the end to show their type
 ls -i #Show the inodes
+echo "hola">test.txt #Creates a file and writes “hola” inside it
+stat test.txt #Shows detailed information about the test.txt file, such as size, permissions, dates, and inode number.
