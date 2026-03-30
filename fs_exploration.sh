2fc3e2f2a0ff0764453d575d41f7a2741c72fe6e
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-03-30 12:59:42 +0000

    fs_exploration_update_upgrade_install

---
+++ fs_exploration.sh
@@ -0,0 +1,6 @@
+sudo apt update # Update the list of available packages from the repositories 
+sudo apt upgrade # Update all installed packages to their latest versions
+sudo apt install parted # Install the "Parted" tool, used to manage disk partitions
+
+#The order to follow is update, upgrade and install
+
