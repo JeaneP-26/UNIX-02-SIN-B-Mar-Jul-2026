c3c473aed154e31d880b8ab6b69ffe4c9e76574d
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-03-30 13:48:34 +0000

    fs_exploration_archivo

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -7,3 +7,6 @@ sudo apt install parted # Install the "Parted" tool, used to manage disk partiti
 sudo parted -l && -e "\n---\n" && lsblk -f && echo -e "\n---\n" # Display partitions and file with separators, print a visual separator, list the block devices with their file systems, and display another separator again to organize the output.
 lsblk -f #Show the storage devices and their file systems
 [-d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #Check if the folder exists; if it does, the test is true
+
+echo "esto es un archivo" >archivo.txt #A file named archivo.txt is created
+stat archivo.txt # Show detailed information about the file (size, permissions, dates, inodes, etc.)
