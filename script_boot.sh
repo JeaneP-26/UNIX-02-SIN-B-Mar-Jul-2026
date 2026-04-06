70bb98f82be8f0520020c0876c57e153f0283217
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-04-06 12:39:22 +0000

    script_boot_Directory_Proyecto

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,12 @@ cd #Changes the current directory to your home directory
 cd /home/codespace #hanges the current directory to the specific path /home/codespace.
 cd ~ #The ~ symbol represents the home directory of the current user
 cd $HOME #$HOME is an environment variable that stores the path to your home directory.
+
+
+mkdir proyecto #Create the directory "Proyecto"
+cd proyecto/ #Change to directory "Proyecto"
+ls -lai #Lists all files and directories in the current directory with detailed information
+#Result of the list:
+#total 12
+#925546 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:34 .
+#918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:34 ..
