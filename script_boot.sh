31cea872d75ceb11503783459843f0835f53e032
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-04-06 12:33:19 +0000

    script_boot_Changes_directory_home_directory

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+cd #Changes the current directory to your home directory
+cd /home/codespace #hanges the current directory to the specific path /home/codespace.
+cd ~ #The ~ symbol represents the home directory of the current user
+cd $HOME #$HOME is an environment variable that stores the path to your home directory.
