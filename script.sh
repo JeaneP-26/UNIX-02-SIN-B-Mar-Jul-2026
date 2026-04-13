264b67750ebf1a4ae3a4313b481e50796e5b2184
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-04-13 12:35:13 +0000

    script_Large_options_And_Short_options

---
+++ script.sh
@@ -0,0 +1,8 @@
+ls -a #Short option
+ls --all #Large Option
+
+ls -a /
+#Command,Option,Arguments
+#Command: (ls) displays the contents of a directory
+#Option: (-a) includes all hidden files
+#Arguments: (/) specifies the root directory of the system
