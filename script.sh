04757ef4a699dc5113fda6284b5b7332f3501821
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-04-13 12:42:14 +0000

    script_List_Files_And_Directories

--- script.sh
+++ script.sh
@@ -6,3 +6,17 @@ ls -a /
 #Command: (ls) displays the contents of a directory
 #Option: (-a) includes all hidden files
 #Arguments: (/) specifies the root directory of the system
+
+
+ls -l # List files in long format (details like permissions, size, date)
+ls -h # Show sizes in human-readable format (KB, MB, GB)
+
+
+#Command,Option,Arguments
+ls -l -a -h
+#Command (ls): lists files and directories
+#Option (-l): shows details such as permissions, owner, size, and date
+#Option (-a): includes hidden files
+#Option (-h): displays sizes in a human-readable format (KB, MB, etc.)
+ls -l -ah #Large Option
+ls -lah #Short Option
