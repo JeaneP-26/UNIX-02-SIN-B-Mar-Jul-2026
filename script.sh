247682f0734716895959751907e12334399463c8
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-04-13 13:06:24 +0000

    script_Depth_Man_Ls

--- script.sh
+++ script.sh
@@ -36,4 +36,11 @@ man ls #Large Option, open the manual page for the ls command
 #N: go back to the previous result
 #q: quit the manual
 
+man git-clone
+/depth
+#Results:
+#--depth <depth>
+    #Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch
+    #unless --no-single-branch is given to fetch the histories near the tips of all branches. If you want to
+    #clone submodules shallowly, also pass --shallow-submodules.
 
