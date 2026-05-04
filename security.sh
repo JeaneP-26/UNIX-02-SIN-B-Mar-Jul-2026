267665bac6033ae43203fd9dfd79525f20aea001
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-05-04 13:39:25 +0000

    Inspect_and_filter_group_information

--- security.sh
+++ security.sh
@@ -37,3 +37,104 @@ groups # Show groups of the current user
 
 groups $USER  # Show groups of the current user using the $USER variable
 #Result: root
+
+id -u # Show user ID (UID)
+id -g # Show primary group ID (GID)
+id -G # Show all group IDs the user belongs to
+
+cat /etc/group  # Display the contents of /etc/group (list of system groups)
+#Results:
+#root:x:0:
+#daemon:x:1:
+#bin:x:2:
+#sys:x:3:
+#adm:x:4:
+#tty:x:5:
+#disk:x:6:
+#lp:x:7:
+#mail:x:8:
+#news:x:9:
+#uucp:x:10:
+#man:x:12:
+#proxy:x:13:
+#kmem:x:15:
+#dialout:x:20:
+#fax:x:21:
+#voice:x:22:
+#cdrom:x:24:
+#floppy:x:25:
+#tape:x:26:
+#sudo:x:27:
+#audio:x:29:
+#dip:x:30:
+#www-data:x:33:
+#backup:x:34:
+#operator:x:37:
+#list:x:38:
+#irc:x:39:
+#src:x:40:
+#shadow:x:42:
+#utmp:x:43:
+#video:x:44:
+#sasl:x:45:
+#plugdev:x:46:
+#staff:x:50:
+#games:x:60:
+#users:x:100:
+#nogroup:x:65534:
+#lpadmin:x:999:
+#systemd-journal:x:998:
+#systemd-network:x:997:
+#debian-tor:x:101:
+#tcpdump:x:996:
+cat /etc/group | grep games
+# Show lines in /etc/group that contain "games"
+# grep filters matching text
+#Result: games:x:60:
+cat /etc/gshadow
+# Display the contents of /etc/gshadow (secure group information)
+# Usually requires root privileges
+#Results:
+#root:*::
+#daemon:*::
+#bin:*::
+#sys:*::
+#adm:*::
+#tty:*::
+#disk:*::
+#lp:*::
+#mail:*::
+#news:*::
+#uucp:*::
+#man:*::
+#proxy:*::
+#kmem:*::
+#dialout:*::
+#fax:*::
+#voice:*::
+#cdrom:*::
+#floppy:*::
+#tape:*::
+#sudo:*::
+#audio:*::
+#dip:*::
+#www-data:*::
+#backup:*::
+#operator:*::
+#list:*::
+#irc:*::
+#src:*::
+#shadow:*::
+#utmp:*::
+#video:*::
+#sasl:*::
+#plugdev:*::
+#staff:*::
+#games:*::
+#users:*::
+#nogroup:*::
+#lpadmin:!*::
+#systemd-journal:!*::
+#systemd-network:!*::
+#debian-tor:!::
+#tcpdump:!*::
