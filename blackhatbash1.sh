9c2050794f6610525862c73650d7f83ba440b993
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-01 13:16:32 +0000

    Toggle Debug Mode

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 #!/bin/bash -x 
 # Run the script in debug mode, displaying each command before executing it
+
+set -x # Activate debug mode within the script
+
 bash --version
 env
 echo ${SHELL}
@@ -11,6 +13,9 @@ echo ${OSTYPE}
 ps -e -f
 ps -ef
 df --human-readable
-bash -r blackhatbash1.sh 
+#bash -r blackhatbash1.sh 
 # Run the script in restricted mode, limiting dangerous commands such as cd or redirects
-bash -n blackhatbash1.sh # Check the script syntax without running it
+#bash -n blackhatbash1.sh # Check the script syntax without running it
+#bash -x blackhatbash1.sh # Run the script, displaying each command and its expansion in real time.
+
+set +x # Desactivate debug mode within the script
