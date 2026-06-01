b5f7054d2b2bf9f448aaedf4e8e80b860389b737
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-01 22:17:37 +0000

    Verify group inheritance on new files

---
+++ security2.sh
@@ -0,0 +1,12 @@
+id # Check current user and group information
+#Result:
+#uid=0(root) gid=0(root) grupos=0(root)
+id -gn # Show only the name of the main group
+#Result:
+#root
+touch ~/test_grupo_heredado.txt #Create test file in home directory
+ls -la ~/test_grupo_heredado.txt #Verify test file permissions
+# Create file and check which group it inherits
+#Result:
+#-rw-r--r-- 1 root root 0 jun  1 22:12 /root/test_grupo_heredado.txt
+# The group is the main group of the current user
