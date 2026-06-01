ae6a51c52a45bedb079ef9c5aab6337c48d39c62
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-01 13:39:58 +0000

    Use Variable Without Braces

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 book="black hat bash"
 echo "This book's name is ${book}"
-bash -x blackhatbash3.sh #Run the script in debug mode
+#bash -x blackhatbash3.sh #Run the script in debug mode
 #Result:
 #+ book='black hat bash'
+#+ echo 'This book'\''s name is black hat bash'
+#This book's name is black hat bash
+
+echo "This book's name is $book"
+#bash -x blackhatbash3.sh #Run the script in debug mode
+#Result:
+#+ book='black hat bash'
+#+ echo 'This book'\''s name is black hat bash'
+#This book's name is black hat bash
 #+ echo 'This book'\''s name is black hat bash'
 #This book's name is black hat bash
