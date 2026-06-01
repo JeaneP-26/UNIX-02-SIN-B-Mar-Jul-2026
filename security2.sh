0caa1d01d9d15e473d966fcad3909638d9e630c7
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-01 22:53:04 +0000

    Create files and Directory inside newgrp subshell

--- security2.sh
+++ security2.sh
@@ -34,3 +34,32 @@ id -gn # Show only the name of the main group
 echo "Nuevo grupo activo: $(id -gn)" # Verify that the active group has changed
 #Result:
 # Nuevo grupo activo: desarrolladores
+
+
+touch ~/dentro_de_newgrp.txt #Create file inside newgrp session
+ls -la ~/dentro_de_newgrp.txt #Verify newgrp file permissions
+# Create a file inside the newgrp subshell
+#Result:
+#-rw-r--r-- 1 root desarrolladores 0 jun  1 22:42 /root/dentro_de_newgrp.txt
+# The file group is now desarrolladores
+mkdir -p ~/proyecto_dev/src #Create project directory structure
+ls -la ~/ #List home folder
+#Result:
+#total 64
+#drwx------ 1 root root             4096 jun  1 22:44 .
+#drwxr-xr-x 1 root root             4096 jun  1 21:42 ..
+#-rw-r--r-- 1 root root             5857 jun  1 21:42 .bashrc
+#-rw-r--r-- 1 root root              607 may 31 04:02 .bashrc.original
+#drwxr-xr-x 3 root root             4096 jun  1 21:42 .cache
+#drwx------ 3 root root             4096 jun  1 21:42 .config
+#-rw-r--r-- 1 root desarrolladores     0 jun  1 22:42 dentro_de_newgrp.txt
+#lrwxrwxrwx 1 root root               47 jun  1 21:42 .docker -> /workspaces/.codespaces/.persistedshare/.docker
+#drwxr-xr-x 3 root root             4096 jun  1 21:43 .dotnet
+#drwx------ 4 root root             4096 jun  1 21:43 .local
+#-rw-r--r-- 1 root root              132 may 15 11:37 .profile
+#drwxr-xr-x 3 root desarrolladores  4096 jun  1 22:44 proyecto_dev
+#drwx------ 2 root root             4096 jun  1 21:42 .ssh
+#-rw-r--r-- 1 root root                0 jun  1 22:12 test_grupo_heredado.txt
+#drwxr-xr-x 5 root root             4096 jun  1 21:42 .vscode-remote
+#-rw-r--r-- 1 root root            10882 may 31 04:02 .zshrc
+# proyecto_dev/ has the group desarrolladores
