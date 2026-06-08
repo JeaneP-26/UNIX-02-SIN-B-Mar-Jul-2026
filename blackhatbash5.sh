3773f1a0c1b62477151af1070143a6e00ce83fe1
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-08 16:36:27 +0000

    Add stdout and stderr redirection examples to separate and combined files

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,4 +3,9 @@
 touch test && touch test123
 (ls; ps)
 ls ; ps; whoami
-lzl || echo "the lzl command failed" 
+lzl || echo "the lzl command failed" i
+
+ls -l / &> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
