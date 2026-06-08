a80e392509b1ebc13b493de06a628d97a7c871ca
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-08 12:51:44 +0000

    Limit ping to 3 packets using -c flag

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -4,11 +4,16 @@ SCRIPT_NAME="${0}"
 TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"8
+ping -c 3 "${TARGET}"  # Send only 3 ping packets to the target using the -c flag
 #Result:
 #Running the script /usr/bin/bash...
 #Pinging the target: ...
 #ping: : Nombre o servicio desconocido
 
-chmod u+x ping_with_arguments.sh # Grant execute permission to the owner of the ping_with_arguments.sh script
-./ping_with_arguments.sh nostarch.com # Execute the ping_with_arguments.sh script with nostarch.com as the argument
+#chmod u+x ping_with_arguments.sh # Grant execute permission to the owner of the ping_with_arguments.sh script
+#./ping_with_arguments.sh nostarch.com # Execute the ping_with_arguments.sh script with nostarch.com as the argument
+
+echo "The arguments are: $@" #Print all arguments passed to the script
+echo "The total number of arguments is: $#" #Print the total number of arguments passed to the script
+
+
