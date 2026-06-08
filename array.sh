2b02aea7f9f406a422d43260cb61f943b0686125
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-08 15:58:20 +0000

    Remove array element using unset and Print remaining elements

--- array.sh
+++ array.sh
@@ -6,4 +6,6 @@ IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
 echo "${IP_ADDRESSES[*]}"
 # Prints only the first element in the array
 echo "${IP_ADDRESSES[0]}"
+unset IP_ADDRESSES[1]  # Remove the second element (index 1) from the IP_ADDRESSES array
+echo "${IP_ADDRESSES[*]}" # Print all remaining elements in the array
 set +x
