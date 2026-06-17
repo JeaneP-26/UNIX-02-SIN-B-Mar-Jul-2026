6ab64ebde4aa21ff19150c0b1ca04d3e40ce26e9
JeaneP-26 <jeaparedesve@uide.edu.ec>
2026-06-17 13:17:54 +0000

    Demonstrate fg and bg commands to manage foreground and background processes

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -62,3 +62,14 @@ jobs
 #Result:
 #[1]+  Ejecutando                 sleep 100 &
 
+fg %1
+#Result:
+#sleep 100
+
+Ctrl+z #To stopped the process
+#^Z
+#[1]+  Detenido                   sleep 100
+
+bg %1
+#Result:
+#[1]+ sleep 100 &
